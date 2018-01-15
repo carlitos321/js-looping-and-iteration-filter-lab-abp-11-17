8f4a5c271579258742a396e0d965b85b2744b871
carlitos321 <ca.cabrera321@gmail.com>
2018-01-15 23:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(drivers, string) {
-
+  if (driver === string) {
+    return true
+  } else {
+    return false
+  }
 }
+
+drivers.filter(findMatching);
 
 function fuzzyMatch(drivers, string) {
 
