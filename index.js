196fb2c8c820add0f263057af3192fee830a9f56
carlitos321 <ca.cabrera321@gmail.com>
2018-01-16 00:04:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(drivers, name) {
   let matchingDrivers = drivers.filter(function(driver){
-    if (name === driver){
+    if (name.toLowerCase() === driver.toLowerCase()){
       return true
     } else {
       return false
